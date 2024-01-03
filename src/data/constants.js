fa034eca22883a9888a56c8930b0a4fe1acfc8c1
tms43 <96717116+tms43@users.noreply.github.com>
2024-01-03 20:46:53 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,301 @@
+export const Bio = {
+  name: "Tanmay Sharma",
+  roles: [
+    "Web Developer",
+    "Data Scientist",
+    "Data Analyst",
+  ],
+  description:
+    "Eager and adaptable, I'm a motivated individual with a passion for learning in the fields of Data Science and Machine Learning. Dedicated to delivering high-quality results, my positive attitude and growth mindset drive me to make meaningful contributions and achieve excellence.",
+    linkedin: "https://www.linkedin.com/in/tanmay-sharma-3131bb1a0/",  
+    github: "https://github.com/tanmay316",
+  resume:
+    "https://drive.google.com/file/d/1cRwaSd7KnVw-JRUHWGoiYynjolJu8VTI/view?usp=sharing",
+  
+  twitter: "https://twitter.com/tms_43",
+};
+
+export const skills = [
+  {
+    title: "Web development",
+    skills: [
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      
+      {
+        name: "Vue.js",
+        image:
+          "https://www.cdnlogo.com/logos/v/92/vue-js.svg",
+      },
+      {
+        name: "Flask",
+        image:
+          "https://www.cdnlogo.com/logos/f/50/flask.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Sqlite",
+        image: "https://www.cdnlogo.com/logos/s/41/sqlite.svg",
+      },
+    ],
+  },
+  {
+    title: "Data Science",
+    skills: [
+      
+      {
+        name: "Machine Learning",
+        image:
+          "https://www.cdnlogo.com/logos/a/18/amazon-artificial-intelligence.svg",
+      },
+      {
+        name: "Deep Learning",
+        image:
+          "https://cdn-images-1.medium.com/max/2600/1*u9L_UJbV0Qfg1PZQkHna2g.png",
+      },
+      {
+        name: "Data Analysis",
+        image:
+          "https://th.bing.com/th/id/OIP.xyk9gQ1zdlsJFTZtTqC8wwHaGT?rs=1&pid=ImgDetMain",
+      },
+      {
+        name: "Data Visualization",
+        image:
+          "https://th.bing.com/th/id/OIP.tQsDHk_1FlzW4QFu0prBGQAAAA?rs=1&pid=ImgDetMain",
+      },
+      {
+        name: "Natural Language Processing (NLP)",
+        image:
+          "https://miro.medium.com/proxy/0*2t3JCdtfsV2M5S_B.png",
+      },
+      {
+        name: "Statistics",
+        image:
+          "https://i0.wp.com/discovermni.com/wp-content/uploads/2018/09/statistics-word-web.jpg?fit=3000%2C1514&ssl=1",
+      },
+    ],
+  },
+  {
+    title: "Programming Languages",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "Javascript",
+        image:
+          "https://www.cdnlogo.com/logos/j/44/javascript.svg",
+      },
+      {
+        name: "Kotlin",
+        image:
+          "https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg",
+      },
+      {
+        name: "XML",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+      },
+      {
+        name: "C++",
+        image:
+          "https://www.cdnlogo.com/logos/c/1/c-plus-plus.svg",
+      },{
+        name: "SQL",
+        image:
+          "https://www.cdnlogo.com/logos/a/2/amazon-database.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "MS Excel",
+        image:
+          "https://www.cdnlogo.com/logos/m/35/microsoft-excel.svg",
+      },
+      {
+        name: "Power BI",
+        image:
+          "https://www.cdnlogo.com/logos/p/48/power-bi.svg",
+      },
+      
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+      {
+        name: "Azure",
+        image:
+          "https://www.cdnlogo.com/logos/a/46/azure.svg",
+      },
+      {
+        name: "AWS",
+        image:
+        "https://www.cdnlogo.com/logos/a/19/aws.svg",
+      },
+      
+    ],
+  },
+];
+
+export const experiences = [
+    {
+    id: 0,
+    role: 'COMING SOON...',
+    },
+  // {
+  //   id: 0,
+  //   img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+  //   role: "DevOps & Fullstack Engineering Intern",
+  //   company: "Flipr Inovations Pvt. Ltd.",
+  //   date: "Aug 2023 - July 2023",
+  //   desc: "Working on Flipr Platforms, managing DevOps, and streamlining the process with automation.",
+  //   skills: [
+  //     "Docker",
+  //     "Terraform",
+  //     "AWS",
+  //     "EC2",
+  //     "Portainer",
+  //     "Nginx",
+  //     "JavaScript",
+  //     "TypeScript",
+  //     "Node Js",
+  //     " Next Js",
+  //   ],
+  //   doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+  // },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://www.cdnlogo.com/logos/i/27/iit-madras.svg",
+    school: "Indian Institute of Technology, Madras",
+    date: "Oct 2021 - Sep 2025",
+    //grade: "7.1 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Data Science at IIT Madras. ",
+    degree: "Bachelor of Science - BS, Data Science and Application",
+  },
+  {
+    id: 1,
+    img: "https://yt3.ggpht.com/a/AATXAJx5T-aaThgfa3_7eG9rFC_we1i7fA8v9l0t73az=s900-c-k-c0xffffffff-no-rj-mo",
+    school: "BK Birla Institute of Engineering and Technology, Pilani",
+    date: "Oct 2021 - Sep 2025",
+    //grade: "8.16 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering. ",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 2,
+    img: "https://th.bing.com/th/id/OIP.p4bLT6cd1yDCkcLElzGrgAHaHa?rs=1&pid=ImgDetMain",
+    school: "Birla Shishu Vihar, Pilani",
+    date: "Apr 2019 - Apr 2020",
+    //grade: "90.0%",
+    desc: "I completed my class 12th education at BSV, Pilani, where I studied Science with Computer Application.",
+    degree: "XII, Science with Computer",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "TmsNirvana",
+    date: "Nov 2023 - Dec 2023",
+    description:
+      "TmsNirvana is a music streaming website which gives music recommendations using the power of machine learning",
+    image:
+      "https://github.com/tanmay316/TmsNirvana/assets/127765354/6559d519-a081-4be0-86e9-196c5b4f5d85",
+    tags: [
+      "Flask",
+      "SQLite",
+      "Machine Learning",
+      "Bootstrap",
+      "Jinja",
+      "Data analytics",
+    ],
+    category: "web app",
+    github: "https://github.com/tanmay316/TmsNirvana",
+    webapp: "https://github.com/tanmay316/TmsNirvana",
+  },
+  
+  {
+    id: 2,
+    title: "Sentiment Analysis",
+    date: "Aug 2023 - Sep 2023",
+    description:
+      "Developed a sentiment analysis model that automatically analyzes and classifies sentiments in movie reviews, achieving an accuracy score of 80.55%",
+    image:
+      "https://github.com/tanmay316/Sentiment-Analysis-/assets/127765354/d4eed06b-a26f-41bd-8296-7e2bb7901223",
+    tags: ["Python", "Sklearn", "Natural Language Processing", "Logistic Regression", " Naïve Bayes", "Django"],
+    category: "machine learning",
+    github: "https://github.com/tanmay316/Sentiment-Analysis-",
+    webapp: "https://github.com/tanmay316/Sentiment-Analysis-",
+    
+  },
+  {
+    id: 3,
+    title: "Sales Prediction",
+    date: "August 2023 - Sep 2023",
+    description:
+      "Developed predictive machine learning models (R-square=0.99) for sales, delivering actionable strategies for inventory,and marketing via a Flask web app integration.",
+    image:
+      "https://github.com/tanmay316/BDM-Sales-prediction/assets/127765354/a798ff82-4554-4ed3-9d85-61a46bc3660f",
+    tags: ["Machine Learning", "XG Boost", "Flask","Predictive model"],
+    category: "machine learning",
+    github: "https://github.com/tanmay316/BDM-Sales-prediction",
+    webapp: "https://tanmay316.github.io/BDM-Sales-prediction/",
+  },
+
+  {
+    id: 3,
+    title: "Enhancing Profitability and Sales Growth",
+    date: "August 2023 - Sep 2023",
+    description:
+      "Utilized the Data Science lifecycle for addressing business challenges, including data collection and cleaning (50,000 rows, 417 columns), exploratory data analysis, feature engineering, predictive modeling, and deployment. Leveraged Excel to create dynamic Sales dashboards and harnessed Power BI for interactive sales and inventory visualizations, driving effective data analysis.",
+    image:
+      "https://github.com/tanmay316/Enhancing-Profitability-and-Sales-Growth-Data-driven-Analysis-and-Strategies/assets/127765354/e4706f42-bf1d-4d25-9e79-6bc4a7be8671",
+    tags: ["Excel", "Power BI", "Financial Data Analytics","Data Visualization","Deriving business insights"],
+    category: "data analysis",
+    github: "https://github.com/tanmay316/Enhancing-Profitability-and-Sales-Growth-Data-driven-Analysis-and-Strategies",
+    webapp: "https://github.com/tanmay316/Enhancing-Profitability-and-Sales-Growth-Data-driven-Analysis-and-Strategies",
+  },
+  
+];
+
+
